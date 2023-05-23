b5f456070ec294a2fa2aaf05841333a9d66f0998
Ludobico <aqs450@gmail.com>
2023-05-23 17:54:08 +0900

    init : Route setting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,15 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import WorldStory from "./components/Canvas/WorldStory";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/test" element={<WorldStory />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
