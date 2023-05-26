14a4ae5223b1b86e7cc5f5230ec787a134620ccb
Ludobico <aqs450@gmail.com>
2023-05-26 17:15:02 +0900

    Add LoaderTransition

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import WorldStory from "./components/Canvas/WorldStory";
 import Main from "./components/Main";
+import LoaderTransition from "./components/Loader/LoaderTransition";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/test" element={<LoaderTransition />} />
       </Routes>
     </div>
   );
---
+++ frontend/src/components/Loader/LoaderTransition.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoaderTransition = () => {
+  return <div></div>;
+};
+
+export default LoaderTransition;
