fbeb66595cbeda60cb5a0f3fd740586610b050c6
chanjin kim <aqs450@gmail.com>
2023-06-10 23:05:28 +0900

    feat : shsader material setting

--- frontend/src/components/Canvas/WorldStory.jsx
+++ frontend/src/components/Canvas/WorldStory.jsx
@@ -2,7 +2,13 @@ import React, { useEffect, useRef, useState } from "react";
 import LensFlare from "./UltimateLensFlare";
 import "./WorldStory.css";
 import { EffectComposer } from "@react-three/postprocessing";
-import { Html, OrbitControls, PerspectiveCamera, Stars, useTexture } from "@react-three/drei";
+import {
+  Html,
+  OrbitControls,
+  PerspectiveCamera,
+  Stars,
+  useTexture,
+} from "@react-three/drei";
 import lensIMG from "../Static/lensDirtTexture.png";
 import background_1 from "../Static/background1.jpg";
 import background_2 from "../Static/background2.jpg";
@@ -11,6 +17,7 @@ import * as THREE from "three";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useFrame } from "@react-three/fiber";
+import TransitionShaderMaterial from "../Shaders/TransitionShader";
 
 function Skybox() {
   const backgroundList = useTexture([background_1, background_2, background_3]);
@@ -19,9 +26,24 @@ function Skybox() {
 
   useFrame((state, delta) => {});
   return (
-    <mesh userData={{ LensFlare: "no-occlusion" }} scale={[-1, 1, 1]} ref={shaderMaterialRef}>
-      <sphereBufferGeometry castShadow={false} receiveShadow={false} args={[5, 64, 64]} />
-      <meshBasicMaterial toneMapped={false} map={backgroundList[textureIndex]} side={THREE.FrontSide} />
+    <mesh
+      userData={{ LensFlare: "no-occlusion" }}
+      scale={[-1, 1, 1]}
+      ref={shaderMaterialRef}
+    >
+      <sphereBufferGeometry
+        castShadow={false}
+        receiveShadow={false}
+        args={[5, 64, 64]}
+      />
+      {/* <meshBasicMaterial toneMapped={false} map={backgroundList[textureIndex]} side={THREE.FrontSide} /> */}
+      <transitionShaderMaterial
+        ref={shaderMaterialRef}
+        uTexture1={backgroundList[0]}
+        uTexture2={backgroundList[1]}
+        uTexture3={backgroundList[2]}
+        attach="material"
+      />
     </mesh>
   );
 }
@@ -57,15 +79,38 @@ const WorldStory = () => {
   return (
     <>
       <OrbitControls ref={OrbitcameraRef} autoRotate enableZoom={false} />
-      <PerspectiveCamera makeDefault position={[-2.129, 0.177, 27.08]} ref={cameraRef} />
+      <PerspectiveCamera
+        makeDefault
+        position={[-2.129, 0.177, 27.08]}
+        ref={cameraRef}
+      />
       <EffectComposer>
         {/* 테스트용 */}
         {/* <LensFlare dirtTextureFile={lensIMG} {...lensFlareProps} /> */}
-        <LensFlare dirtTextureFile={lensIMG} colorGain={new THREE.Color(56, 22, 11)} opacity={0.8} flareShape={0.37} flareSize={0.004} flareSpeed={0.4} glareSize={0.01} starPoints={0.1} ghostScale={0.1} haloScale={0.5} />
+        <LensFlare
+          dirtTextureFile={lensIMG}
+          colorGain={new THREE.Color(56, 22, 11)}
+          opacity={0.8}
+          flareShape={0.37}
+          flareSize={0.004}
+          flareSpeed={0.4}
+          glareSize={0.01}
+          starPoints={0.1}
+          ghostScale={0.1}
+          haloScale={0.5}
+        />
       </EffectComposer>
       <directionalLight intensity={1} position={[0, 0, 0]} />
       <Skybox />
-      <Stars radius={100} depth={50} count={3000} factor={4} saturation={0} fade speed={1} />
+      <Stars
+        radius={100}
+        depth={50}
+        count={3000}
+        factor={4}
+        saturation={0}
+        fade
+        speed={1}
+      />
       <Html fullscreen wrapperClass="Introduce_top_div" zIndexRange={[100, 0]}>
         <Introduce />
       </Html>
