631e556fabaece044ffddf7da721090ee58a8855
Ludobico <aqs450@gmail.com>
2023-06-28 17:55:08 +0900

    feat : chain connection

---
+++ backend/Module/CharacterSettingCT.py
@@ -0,0 +1,59 @@
+from langchain.llms import LlamaCpp
+from langchain import PromptTemplate, LLMChain
+from langchain.prompts.chat import ChatPromptTemplate, SystemMessagePromptTemplate, AIMessagePromptTemplate, HumanMessagePromptTemplate
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.memory import ConversationBufferMemory
+from ctransformers.langchain import CTransformers
+
+
+class CharacterSettingLangchain_CTransformers:
+    def llm_connect(self):
+        callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+        config = {'stream': True, 'batch_size': 8,
+                  'max_new_tokens': 512, 'temperature': 0.95}
+        llm = CTransformers(model='../Models/WizardLM-13B-1.0.ggmlv3.q4_0.bin',
+                            model_type='llama', config=config, callback_manager=callback_manager)
+        return llm
+
+    def prompt_setting(self):
+        template = """
+    {instruct}
+
+    Character Name:
+    Gender:
+    Age:
+    Personality:
+    Background:
+    Dialogue Style:
+    Appearance:
+
+    Ensure your responses are consistent with the world and setting of your story. Be creative and feel free to include any relevant details that will help the model generate a rich and unique character description. Provide as much information as possible to make the character come to life within the story you have in mind.
+    create a character for a story set in various settings such as historical, futuristic, fantasy,modern or science fiction.
+    Let's think step by step.
+
+    Provide a JSON-formatted response with information about a person. Include the following fields: Character Name, Gender, Age, Personality, Background, Dialogue Style and Appearance. Do not create any keys except for the 7 keys above
+
+    writer : 
+
+    """
+        prompt = PromptTemplate(
+            template=template, input_variables=["instruct"])
+        return prompt
+
+    def llm_prompt_chain(self):
+        llm = self.llm_connect()
+        prompt = self.prompt_setting()
+        llm_chain = LLMChain(prompt=prompt, llm=llm)
+        return llm_chain
+
+    def run_llm_chain(self):
+        text = "You are a talented writer creating a character for a story. Provide detailed information for the following aspects of your character:"
+        llm_chain = self.llm_prompt_chain()
+        response = llm_chain.run(text)
+
+
+if __name__ == "__main__":
+    CSL = CharacterSettingLangchain()
+    CSL.run_llm_chain()
---
+++ backend/main.py
@@ -0,0 +1,5 @@
+from fastapi import FastAPI
+from Module.CharacterSetting import CharacterSettingLangchain
+
+app = FastAPI()
+CSL = CharacterSettingLangchain()
