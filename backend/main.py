5b91264bf393b1516a8401738dc55f61933a1921
Ludobico <aqs450@gmail.com>
2023-08-18 14:56:23 +0900

    test : streaming callback handler test

--- backend/main.py
+++ backend/main.py
@@ -1,75 +1,96 @@
 import asyncio
 import os
-from typing import AsyncIterable, Awaitable, Callable, Optional, Union, Any
-from uuid import UUID
+from typing import AsyncIterable, Awaitable, Callable, Union, Any
 
 import uvicorn
-# 스트리밍 output
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 from langchain.callbacks import AsyncIteratorCallbackHandler
 from langchain.callbacks.base import AsyncCallbackHandler
-from langchain.callbacks.manager import AsyncCallbackManagerForLLMRun
 from pydantic import BaseModel
+from uuid import UUID
 
-# LLM
 from langchain.llms import LlamaCpp
 from langchain import PromptTemplate, LLMChain
-from langchain.callbacks.manager import CallbackManager
-from ctransformers.langchain import CTransformers
+from langchain.callbacks.manager import CallbackManager, AsyncCallbackManagerForLLMRun
+
+# Load env variables from .env file
+load_dotenv()
 
 app = FastAPI()
 
-template = """
-Question : {question}
 
-Answer : Let's work this out in a step by step way to be sure we habe the right answer.
-"""
+template = """Question: {question}
+
+Answer: Let's work this out in a step by step way to be sure we have the right answer."""
 
 prompt = PromptTemplate(template=template, input_variables=["question"])
+
 
 Sender = Callable[[Union[str, bytes]], Awaitable[None]]
 
 
 class AsyncStreamCallbackHandler(AsyncCallbackHandler):
+    """Callback handler for streaming, inheritance from AsyncCallbackHandler."""
+
     def __init__(self, send: Sender):
         super().__init__()
         self.send = send
 
-    async def on_llm_new_token(self, token: str, run_id: UUID, parent_run_id: UUID, **kwargs):
-        await self.send(f"data : {token} \n\n")
+    async def on_llm_new_token(self, token: str, **kwargs: Any) -> None:
+        """Rewrite on_llm_new_token to send token to client."""
+        await self.send(f"data: {token}\n\n")
 
 
 async def send_message(message: str) -> AsyncIterable[str]:
+    # Callbacks support token-wise streaming
     callback = AsyncIteratorCallbackHandler()
-    callback_manager = CallbackManager([callback])
+    cb = AsyncCallbackHandler()
+    print('cbcb {0}'.format(cb))
+    # callback_manager = CallbackManager([callback])
+    cm = AsyncCallbackManagerForLLMRun(cb)
+    # Verbose is required to pass to the callback manager
 
-    llm = LlamaCpp(model_path="./Models/WizardLM-13B-1.0.ggmlv3.q4_0.bin",
-                   callbacks=[callback], verbose=True, streaming=True)
-    llm_chain = LLMChain(prompt=prompt, llm=llm,)
+    # Make sure the model path is correct for your system!
+    llm = LlamaCpp(
+        # replace with your model path
+        model_path="./Models/WizardLM-13B-1.0.ggmlv3.q4_0.bin",
+        callback_manager=None,
+        verbose=True,
+        streaming=True,
+    )
 
-    question = "What NFL team won the Super Bowl in the year Justin Bierber was born?"
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
+
+    question = "What NFL team won the Super Bowl in the year Justin Bieber was born?"
 
     async def wrap_done(fn: Awaitable, event: asyncio.Event):
+        """Wrap an awaitable with an event to signal when it's done or an exception is raised."""
         try:
             await fn
         except Exception as e:
-            print("Caught exception : {0}".format(e))
+            # TODO: handle exception
+            print(f"Caught exception: {e}")
         finally:
+            # Signal the aiter to stop.
             event.set()
 
+    # Begin a task that runs in the background.
     task = asyncio.create_task(wrap_done(
-        llm_chain.arun(message),
-        callback.done
-    ))
+        llm_chain.arun(question),
+        callback.done),
+    )
 
     async for token in callback.aiter():
+        # Use server-sent-events to stream the response
         yield f"data: {token}\n\n"
 
     await task
 
 
 class StreamRequest(BaseModel):
+    """Request body for streaming."""
     message: str
 
 
---
+++ backend/mainl2.py
@@ -0,0 +1,86 @@
+import asyncio
+import os
+from typing import AsyncIterable, Awaitable, Callable, Optional, Union, Any
+from uuid import UUID
+
+import uvicorn
+# 스트리밍 output
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+
+# 콜백 핸들러
+from langchain.callbacks import AsyncIteratorCallbackHandler
+from langchain.callbacks.base import AsyncCallbackHandler
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.manager import AsyncCallbackManager
+
+from pydantic import BaseModel
+
+# LLM
+from langchain.llms import LlamaCpp
+from langchain import PromptTemplate, LLMChain
+from ctransformers.langchain import CTransformers
+
+app = FastAPI()
+
+template = """
+Question : {question}
+
+Answer : Let's work this out in a step by step way to be sure we habe the right answer.
+"""
+
+prompt = PromptTemplate(template=template, input_variables=["question"])
+
+Sender = Callable[[Union[str, bytes]], Awaitable[None]]
+
+
+class AsyncStreamCallbackHandler(AsyncCallbackHandler):
+    def __init__(self, send: Sender):
+        super().__init__()
+        self.send = send
+
+    async def on_llm_new_token(self, token: str, run_id: UUID, parent_run_id: UUID, **kwargs):
+        await self.send(f"data : {token} \n\n")
+
+
+async def send_message(message: str) -> AsyncIterable[str]:
+    callback = AsyncIteratorCallbackHandler()
+    callback_manager = CallbackManager([callback])
+
+    llm = LlamaCpp(model_path="./Models/WizardLM-13B-1.0.ggmlv3.q4_0.bin",
+                   callback_manager=callback_manager, verbose=True, streaming=True)
+    llm_chain = LLMChain(prompt=prompt, llm=llm,)
+
+    question = "What NFL team won the Super Bowl in the year Justin Bierber was born?"
+
+    async def wrap_done(fn: Awaitable, event: asyncio.Event):
+        try:
+            await fn
+        except Exception as e:
+            print("Caught exception : {0}".format(e))
+        finally:
+            event.set()
+
+    task = asyncio.create_task(wrap_done(
+        llm_chain.arun(message),
+        callback.done
+    ))
+
+    async for token in callback.aiter():
+        yield f"data: {token}\n\n"
+
+    await task
+
+
+class StreamRequest(BaseModel):
+    message: str
+
+
+@app.post("/stream")
+def stream(body: StreamRequest):
+    return StreamingResponse(send_message(body.message), media_type="text/event-stream")
+
+
+if __name__ == "__main__":
+    uvicorn.run(host="0.0.0.0", port=8000, app=app)
