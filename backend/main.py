ff45539b5e115f92aa23a03000ccfce1c17f69c5
Ludobico <aqs450@gmail.com>
2023-08-24 12:12:45 +0900

    test : streaming test

--- backend/main.py
+++ backend/main.py
@@ -49,61 +49,23 @@ tracemalloc.start()
 async def hello_world():
     return {'message': "hello world"}
 
-    # template = """Question: {question}
 
-    # Answer: Let's work this out in a step by step way to be sure we have the right answer."""
-
-    # prompt = PromptTemplate(template=template, input_variables=["question"])
-
-    # callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
-    # llm = LlamaCpp(model_path="./Models/WizardLM-13B-1.0.ggmlv3.q4_0.bin",
-    #                callback_manager=callback_manager, verbose=True, streaming=True, max_tokens=25)
-    # llm_chain = LLMChain(prompt=prompt, llm=llm,)
-
-    # question = "What NFL team won the Super Bowl in the year Justin Bierber was born?"
-
-
-class Message(BaseModel):
-    content: str
-
-
-async def send_message(content: str) -> AsyncIterable[str]:
-    callback = AsyncIteratorCallbackHandler()
+@app.get('/stream_chat')
+def stream_chat():
     template = """Question: {question}
 
     Answer: Let's work this out in a step by step way to be sure we have the right answer."""
 
     prompt = PromptTemplate(template=template, input_variables=["question"])
 
+    callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
     llm = LlamaCpp(model_path="./Models/WizardLM-13B-1.0.ggmlv3.q4_0.bin",
-                   callbacks=[callback], verbose=True, streaming=True, max_tokens=25)
-    llm_chain = LLMChain(prompt=prompt, llm=llm, verbose=True)
-    print("content check \n {0}".format(content))
-    task = asyncio.create_task(
-        llm_chain.arun(content)
+                   callbacks=[StreamingStdOutCallbackHandler()], verbose=True, streaming=True, max_tokens=25)
+    llm_chain = LLMChain(prompt=prompt, llm=llm,)
 
-    )
-
-    try:
-        async for token in callback.aiter():
-            yield token
-    except Exception as e:
-        print(f"Caught exception: {e}")
-    finally:
-        callback.done.set()
-
-    await task
-
-
-@app.post("/stream_chat")
-async def stream_chat(message: Message):
-    generator = send_message(message.content)
-    return StreamingResponse(generator, media_type="text/event-stream")
-
-
-class StreamRequest(BaseModel):
-    """Request body for streaming."""
-    message: str
+    question = "What NFL team won the Super Bowl in the year Justin Bierber was born?"
+    response = llm_chain.run(question)
+    return StreamingResponse(response, media_type='text/event-stream')
 
 
 if __name__ == "__main__":
