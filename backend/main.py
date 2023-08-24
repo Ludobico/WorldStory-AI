5ecb3896ab9b220d3c19c2f025238528745247dd
Ludobico <aqs450@gmail.com>
2023-08-24 14:56:08 +0900

    back

--- backend/main.py
+++ backend/main.py
@@ -1,34 +1,21 @@
-import threading
-import queue
 import asyncio
-import uvicorn
-import time
+import os
 from typing import AsyncIterable, Awaitable, Callable, Union, Any
-from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
-from fastapi.responses import StreamingResponse
-from langchain.callbacks import AsyncIteratorCallbackHandler
-from langchain.chat_models import ChatOpenAI
-from langchain.callbacks.manager import CallbackManager
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from langchain.schema import (
-    AIMessage,
-    HumanMessage,
-    SystemMessage
-)
-from websockets.exceptions import ConnectionClosedOK
-from langchain.chains import ConversationChain
-from callback import StreamingLLMCallbackHandler
-from query_data import get_chain
-from sceemas import ChatResponse
-import logging
-from pydantic import BaseModel
-import tracemalloc
+
 import uvicorn
 from dotenv import load_dotenv
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+from langchain.callbacks import AsyncIteratorCallbackHandler
+from langchain.callbacks.base import AsyncCallbackHandler
+from pydantic import BaseModel
+
 from langchain.llms import LlamaCpp
 from langchain import PromptTemplate, LLMChain
-from fastapi import FastAPI
+from langchain.callbacks.manager import CallbackManager
 from fastapi.middleware.cors import CORSMiddleware
+import tracemalloc
+import uvicorn
 # Load env variables from .env file
 load_dotenv()
 
@@ -51,74 +38,81 @@ app.add_middleware(
 tracemalloc.start()
 
 
-@app.get('/')
-async def hello_world():
-    return {'message': "hello world"}
+template = """Question: {question}
+
+Answer: Let's work this out in a step by step way to be sure we have the right answer."""
+
+prompt = PromptTemplate(template=template, input_variables=["question"])
 
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    stream_handler = StreamingLLMCallbackHandler(websocket)
-    qa_chain = get_chain(stream_handler)
+Sender = Callable[[Union[str, bytes]], Awaitable[None]]
 
-    while True:
+
+class AsyncStreamCallbackHandler(AsyncCallbackHandler):
+    """Callback handler for streaming, inheritance from AsyncCallbackHandler."""
+
+    def __init__(self, send: Sender):
+        super().__init__()
+        self.send = send
+
+    async def on_llm_new_token(self, token: str, **kwargs: Any) -> None:
+        """Rewrite on_llm_new_token to send token to client."""
+        await self.send(f"data: {token}\n\n")
+
+
+async def send_message(message: str) -> AsyncIterable[str]:
+    # Callbacks support token-wise streaming
+    callback = AsyncIteratorCallbackHandler()
+    callback_manager = CallbackManager([callback])
+    # Verbose is required to pass to the callback manager
+
+    # Make sure the model path is correct for your system!
+    llm = LlamaCpp(
+        # replace with your model path
+        model_path="./Models/WizardLM-13B-1.0.ggmlv3.q4_0.bin",
+        callback_manager=callback_manager,
+        verbose=True,
+        streaming=True,
+        max_tokens=64
+    )
+
+    llm_chain = LLMChain(prompt=prompt, llm=llm, verbose=True)
+
+    question = "What NFL team won the Super Bowl in the year Justin Bieber was born?"
+
+    async def wrap_done(fn: Awaitable, event: asyncio.Event):
+        """Wrap an awaitable with an event to signal when it's done or an exception is raised."""
         try:
-            # Receive and send back the client message
-            user_msg = await websocket.receive_text()
-            resp = ChatResponse(
-                sender="human", message=user_msg, type="stream")
-            await websocket.send_json(resp.dict())
-
-            # Construct a response
-            start_resp = ChatResponse(sender="bot", message="", type="start")
-            await websocket.send_json(start_resp.dict())
-
-            # Send the message to the chain and feed the response back to the client
-            output = await qa_chain.acall(
-                {
-                    "input": user_msg,
-                }
-            )
-
-            # Send the end-response back to the client
-            end_resp = ChatResponse(sender="bot", message="", type="end")
-            await websocket.send_json(end_resp.dict())
-        except WebSocketDisconnect:
-            logging.info("WebSocketDisconnect")
-            # TODO try to reconnect with back-off
-            break
-        except ConnectionClosedOK:
-            logging.info("ConnectionClosedOK")
-            # TODO handle this?
-            break
+            await fn
         except Exception as e:
-            logging.error(e)
-            resp = ChatResponse(
-                sender="bot",
-                message="Sorry, something went wrong. Try again.",
-                type="error",
-            )
-            await websocket.send_json(resp.dict())
+            # TODO: handle exception
+            print(f"Caught exception: {e}")
+        finally:
+            # Signal the aiter to stop.
+            event.set()
+
+    # Begin a task that runs in the background.
+    task = asyncio.create_task(wrap_done(
+        llm_chain.arun(question),
+        callback.done),
+    )
+
+    async for token in callback.aiter():
+        # Use server-sent-events to stream the response
+        yield f"data: {token}\n\n"
+
+    await task
 
 
-# @app.get('/stream_chat')
-# def stream_chat():
-#     template = """Question: {question}
+class StreamRequest(BaseModel):
+    """Request body for streaming."""
+    message: str
 
-#     Answer: Let's work this out in a step by step way to be sure we have the right answer."""
 
-#     prompt = PromptTemplate(template=template, input_variables=["question"])
-
-#     callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
-#     llm = LlamaCpp(model_path="./Models/WizardLM-13B-1.0.ggmlv3.q4_0.bin",
-#                    callbacks=[StreamingStdOutCallbackHandler()], verbose=True, streaming=True, max_tokens=25)
-#     llm_chain = LLMChain(prompt=prompt, llm=llm,)
-
-#     question = "What NFL team won the Super Bowl in the year Justin Bierber was born?"
-#     response = llm_chain.run(question)
-#     return StreamingResponse(response, media_type='text/event-stream')
+@app.post("/stream")
+def stream(body: StreamRequest):
+    return StreamingResponse(send_message(body.message), media_type="text/event-stream")
 
 
 if __name__ == "__main__":
-    uvicorn.run(host="0.0.0.0", port=8000)
+    uvicorn.run(host="0.0.0.0", port=8000, app=app)
