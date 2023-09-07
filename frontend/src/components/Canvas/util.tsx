a9e3eb89cbdced26dcee96f7d7bd03b02d0e0409
Ludobico <aqs450@gmail.com>
2023-09-07 21:01:16 +0900

    feat : react three fiber optimization

--- frontend/src/components/Canvas/util.tsx
+++ frontend/src/components/Canvas/util.tsx
@@ -1,6 +1,6 @@
 // File copy pasted from https://github.com/pmndrs/react-postprocessing/blob/master/src/util.tsx
 
-import React, { forwardRef, useMemo, useLayoutEffect, MutableRefObject } from 'react'
+import React, { forwardRef, useMemo, useLayoutEffect, MutableRefObject, useState } from 'react'
 import { Vector2, Object3D } from 'three'
 import { ReactThreeFiber, useThree } from '@react-three/fiber'
 import { Effect, BlendFunction } from 'postprocessing'
@@ -8,7 +8,8 @@ import { Effect, BlendFunction } from 'postprocessing'
 type ObjectRef = MutableRefObject<Object3D>
 type DefaultProps = Partial<{ blendFunction: BlendFunction; opacity: number }>
 
-const isRef = (ref: any): ref is ObjectRef => !!ref.current
+// const isRef = (ref: any): ref is ObjectRef => !!ref.current
+const isRef = (ref: any): ref is ObjectRef => process.env.NODE_ENV === 'development' && !!ref.current;
 
 export const resolveRef = (ref: Object3D | ObjectRef) => (isRef(ref) ? ref.current : ref)
 
@@ -21,13 +22,19 @@ export const wrapEffect = <T extends new (...args: any[]) => Effect>(
     ref
   ) {
     const invalidate = useThree((state) => state.invalidate)
-    const effect: Effect = useMemo(() => new effectImpl(props), [props])
+    // const effect: Effect = useMemo(() => new effectImpl(props), [props])
+    const [effect] = useState(() => new effectImpl(props));
 
+    // useLayoutEffect(() => {
+    //   effect.blendMode.blendFunction = !blendFunction && blendFunction !== 0 ? defaultBlendMode : blendFunction
+    //   if (opacity !== undefined) effect.blendMode.opacity.value = opacity
+    //   invalidate()
+    // }, [blendFunction, effect.blendMode, opacity])
     useLayoutEffect(() => {
       effect.blendMode.blendFunction = !blendFunction && blendFunction !== 0 ? defaultBlendMode : blendFunction
       if (opacity !== undefined) effect.blendMode.opacity.value = opacity
       invalidate()
-    }, [blendFunction, effect.blendMode, opacity])
+    }, [blendFunction, opacity, defaultBlendMode, invalidate]);
     return <primitive ref={ref} object={effect} dispose={null} />
   })
 
--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,10 +1,10 @@
 import { Canvas } from "@react-three/fiber";
-import React from "react";
+import {React, useState} from "react";
 import WorldStory from "./Canvas/WorldStory";
 import Header from "./Header/Header";
 import "./Main.css";
 import "./Header/Header.css";
-import { Html, Scroll, ScrollControls } from "@react-three/drei";
+import { Html, PerformanceMonitor, Scroll, ScrollControls } from "@react-three/drei";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import LoaderTransition from "./Loader/LoaderTransition";
@@ -14,14 +14,17 @@ import Logo from "./Header/Logo";
 
 const Main = () => {
   gsap.registerPlugin(ScrollTrigger);
+  const [dpr, setDpr] = useState(1)
   return (
     <div className="Main_top_div">
-      <Canvas gl={{ alpha: false, stencil: false, antialias: false, depth: false }} dpr={1} style={{ width: "100vw", height: "100vh" }}>
+      <Canvas gl={{ alpha: false, stencil: false, antialias: false, depth: false }} dpr={dpr} style={{ width: "100vw", height: "100vh" }} frameloop="demand">
+        <PerformanceMonitor onIncline={() => setDpr(2)} onDecline={() => setDpr(0.5)}>
         <Html fullscreen>
           {/* <Header />
           <Logo /> */}
         </Html>
         <WorldStory />
+        </PerformanceMonitor>
       </Canvas>
       <div className="inside">
         <InsideStory />
