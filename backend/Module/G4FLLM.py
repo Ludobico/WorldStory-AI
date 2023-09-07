2471a41046d65012fcc24a0fa30b6521fd3bbf24
Ludobico <aqs450@gmail.com>
2023-09-07 09:20:06 +0900

    feat : add acall function

--- backend/Module/G4FLLM.py
+++ backend/Module/G4FLLM.py
@@ -1,9 +1,10 @@
-from typing import Any, List, Mapping, Optional, Union
+from typing import Any, Coroutine, List, Mapping, Optional, Union
+from functools import partial
 
 from g4f import ChatCompletion
 from g4f.models import Model
 from g4f.Provider.base_provider import BaseProvider
-from langchain.callbacks.manager import CallbackManagerForLLMRun
+from langchain.callbacks.manager import AsyncCallbackManagerForLLMRun, CallbackManagerForLLMRun
 from langchain.llms.base import LLM
 from langchain.llms.utils import enforce_stop_tokens
 
@@ -42,6 +43,20 @@ class G4FLLM(LLM):
         text = text if type(text) is str else "".join(text)
         if stop is not None:
             text = enforce_stop_tokens(text, stop)
+        return text
+    
+    async def _acall(self, prompt: str, stop: Optional[List[str]] = None, run_manager: Optional[AsyncCallbackManagerForLLMRun] = None, **kwargs: Any) -> str:
+        create_kwargs = {} if self.create_kwargs is None else self.create_kwargs.copy()
+        create_kwargs["model"] = self.model
+        text_callback = None
+        if run_manager:
+            text_callback = partial(run_manager.on_llm_new_token)
+        
+        text = ""
+        for token in ChatCompletion.create(messages=[{"role": "user", "content": prompt}], stream=True, **create_kwargs):
+            if text_callback:
+                await text_callback(token)
+            text += token
         return text
 
     @property
