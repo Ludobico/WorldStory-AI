aad839b1f70f4bc5d47ea861c4fba8e86eb77a28
Ludobico <aqs450@gmail.com>
2023-09-08 09:53:42 +0900

    feat : react three fiber optimization

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,22 +1,26 @@
-import { Canvas } from "@react-three/fiber";
-import React from "react";
-import WorldStory from "./Canvas/WorldStory";
-import Header from "./Header/Header";
-import "./Main.css";
-import "./Header/Header.css";
-import { Html, Scroll, ScrollControls } from "@react-three/drei";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import LoaderTransition from "./Loader/LoaderTransition";
-import InsideStory from "./Canvas/InsideStory";
-import "./Canvas/InsideStory.css";
-import Logo from "./Header/Logo";
+import { Canvas } from '@react-three/fiber';
+import React from 'react';
+import WorldStory from './Canvas/WorldStory';
+import Header from './Header/Header';
+import './Main.css';
+import './Header/Header.css';
+import { Html, Scroll, ScrollControls } from '@react-three/drei';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import LoaderTransition from './Loader/LoaderTransition';
+import InsideStory from './Canvas/InsideStory';
+import './Canvas/InsideStory.css';
+import Logo from './Header/Logo';
 
 const Main = () => {
   gsap.registerPlugin(ScrollTrigger);
   return (
     <div className="Main_top_div">
-      <Canvas gl={{ alpha: false, stencil: false, antialias: false, depth: false }} dpr={1} style={{ width: "100vw", height: "100vh" }}>
+      <Canvas
+        gl={{ alpha: false, stencil: false, antialias: false, depth: false }}
+        dpr={1}
+        style={{ width: '100vw', height: '100vh' }}
+      >
         <Html fullscreen>
           {/* <Header />
           <Logo /> */}
