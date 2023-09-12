bb810144435ee2906e92b4471a084b8dc469cf34
Ludobico <aqs450@gmail.com>
2023-09-12 15:33:08 +0900

    style : folder path change

--- backend/Module/MakeCharacter.py
+++ backend/Module/MakeCharacter.py
@@ -2,13 +2,13 @@ import os
 
 def make_char_folder(name, prompt):
   cur_dir = os.getcwd()
-  char_folder = os.path.join(cur_dir, 'Characters')
-  print('name test : {0}'.format(name))
-  print('prompt test : {0}'.format(prompt))
+  char_folder = os.path.join(cur_dir, 'Characters', name)
 
-  file_name = name + '.txt'
-  file_path = os.path.join(char_folder, file_name)
+  if not os.path.exists(char_folder):
+    os.makedirs(char_folder)
 
-  with open(file_path, 'w', encoding='utf-8') as f:
+  file_name = 'prompt.txt'
+  prompt_file_path = os.path.join(char_folder, file_name)
+
+  with open(prompt_file_path, 'w', encoding='utf-8') as f:
     f.write(prompt)
-    
