93e2bd31c0052ff3c4763bc0373d82483359d1e0
Ludobico <aqs450@gmail.com>
2023-09-13 14:58:59 +0900

    init : chat design build

---
+++ frontend/src/components/Chat/CharacterChat.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './CharacterChat.css';
+
+const CharacterChat = () => {
+  return <div className="CharacterChat_top_div"></div>;
+};
+
+export default CharacterChat;
