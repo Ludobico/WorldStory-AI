2e2185267d10c2bbe4387fbb9ced6856f440254e
Ludobico <aqs450@gmail.com>
2023-09-14 18:02:00 +0900

    feat : basic chat ui

--- frontend/src/components/Chat/ChatMessage.jsx
+++ frontend/src/components/Chat/ChatMessage.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import './ChatMessage.css';
+import giga from '../Static/maxresdefault.jpg';
 
 const ChatMessage = () => {
-  return <div className="chat_message">message</div>;
+  return (
+    <div className="chat_message chat_owner">
+      <div className="chat_message_info">
+        <img src={giga} alt="" />
+        <span>just now</span>
+      </div>
+      <div className="chat_message_content">
+        <p>hi</p>
+        {/* <img src={giga} /> */}
+      </div>
+    </div>
+  );
 };
 
 export default ChatMessage;
