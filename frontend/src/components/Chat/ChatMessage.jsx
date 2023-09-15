474d1bee5118b988f8dc9a8205c55f0ddbda9c56
Ludobico <aqs450@gmail.com>
2023-09-15 10:03:15 +0900

    feat : basic chat ui build

--- frontend/src/components/Chat/ChatMessage.jsx
+++ frontend/src/components/Chat/ChatMessage.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import './ChatMessage.css';
+import './ChatMessage.scss';
 import giga from '../Static/maxresdefault.jpg';
 
 const ChatMessage = () => {
   return (
-    <div className="chat_message chat_owner">
-      <div className="chat_message_info">
-        <img src={giga} alt="" />
-        <span>just now</span>
+    <>
+      <div className="chat_message chat_owner">
+        <div className="chat_message_info">
+          <img src={giga} alt="" />
+          <span>just now</span>
+        </div>
+        <div className="chat_message_content">
+          <p>hi</p>
+          {/* <img src={giga} /> */}
+        </div>
       </div>
-      <div className="chat_message_content">
-        <p>hi</p>
-        {/* <img src={giga} /> */}
-      </div>
-    </div>
+    </>
   );
 };
 
--- frontend/src/components/Chat/Input.jsx
+++ frontend/src/components/Chat/Input.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Input.css';
+import './Input.scss';
 import { SendOutlined } from '@ant-design/icons';
 
 const Input = () => {
