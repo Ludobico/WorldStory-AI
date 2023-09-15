40039d42d7f9e2eaaf7510bfff6dea783d93c915
Ludobico <aqs450@gmail.com>
2023-09-15 15:25:30 +0900

    feat : chat llm setting

--- frontend/src/components/Chat/CharacterChat.jsx
+++ frontend/src/components/Chat/CharacterChat.jsx
@@ -18,17 +18,6 @@ function getItem(label, key, icon, children) {
   };
 }
 
-const sliderMenuItem = {
-  key: 'sliderKey', // 고유한 키값
-  label: 'Slider Label', // 메뉴 아이템 라벨
-  children: (
-    <Slider
-      defaultValue={50} // 초기값 설정
-      tooltip={{ open: true }}
-    />
-  ),
-};
-
 const CharacterChat = () => {
   // 슬라이드 setting
   const [collapsed, setCollapsed] = useState(false);
@@ -56,12 +45,60 @@ const CharacterChat = () => {
   // CharacterItems 상태
   const [characterItems, setCharacterItems] = useState([
     getItem('Character', 'sub1', <UserOutlined />, [getItem('Tom', '3'), getItem('Bill', '4'), getItem('Alex', '5')]),
-    // getItem('Setting', 'sub2', <SettingOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
   ]);
 
   // LLM setting
-  const [llmSetting, setLlmSetting] = useState([getItem('Setting', 'sub2', <SettingOutlined />)]);
+  const [top_k, setTop_k] = useState(40);
+  const [top_p, setTop_p] = useState(0.95);
+  const [temperature, setTemperature] = useState(0.8);
+  const [last_n_tokens, setLast_n_tokens] = useState(64);
+  const [max_new_tokens, setMax_new_tokens] = useState(256);
+  const [gpu_layers, setGpu_layers] = useState(0);
 
+  const handleChange_top_k = (value) => {
+    setTop_k(value);
+  };
+  const handleChange_top_p = (value) => {
+    setTop_p(value);
+  };
+  const handleChange_temperature = (value) => {
+    setTemperature(value);
+  };
+  const handleChange_last_n_tokens = (value) => {
+    setLast_n_tokens(value);
+  };
+  const handleChange_max_new_tokens = (value) => {
+    setMax_new_tokens(value);
+  };
+  const handleChange_gpu_layers = (value) => {
+    setGpu_layers(value);
+  };
+  const [llmSetting, setLlmSetting] = useState([
+    getItem('Setting', 'sub2', <SettingOutlined />, [
+      getItem('top_k', 'top_k', null, [
+        getItem(<Slider defaultValue={top_k} onChange={handleChange_top_k} min={5} max={80} />),
+      ]),
+      getItem('top_p', 'top_p', null, [
+        getItem(<Slider defaultValue={top_p} onChange={handleChange_top_k} min={0} max={1} step={0.01} />),
+      ]),
+      getItem('temperature', 'temperature', null, [
+        getItem(<Slider defaultValue={temperature} onChange={handleChange_temperature} min={0} max={1} step={0.01} />),
+      ]),
+      getItem('last_n_tokens', 'last_n_tokens', null, [
+        getItem(
+          <Slider defaultValue={last_n_tokens} onChange={handleChange_last_n_tokens} min={0} max={1024} step={12} />
+        ),
+      ]),
+      getItem('max_new_tokens', 'max_new_tokens', null, [
+        getItem(
+          <Slider defaultValue={max_new_tokens} onChange={handleChange_max_new_tokens} min={0} max={4096} step={256} />
+        ),
+      ]),
+      getItem('gpu_layers', 'gpu_layers', null, [
+        getItem(<Slider defaultValue={gpu_layers} onChange={handleChange_gpu_layers} min={0} max={16} />),
+      ]),
+    ]),
+  ]);
   // 선택된 모델
   const handleModelSelet = (item) => {
     setSelectedModelItem([getItem(item.key, item.key, <SELECT_MODEL_LOGO />)]);
@@ -98,7 +135,7 @@ const CharacterChat = () => {
           <Menu theme={lightTheme} mode="inline" items={selectedModelItem} selectable={false} />
           <Menu theme={lightTheme} mode="inline" items={modelItems} onClick={handleModelSelet} />
           <Menu theme={lightTheme} mode="inline" items={characterItems} />
-          <Menu theme={lightTheme} mode="vertical" items={llmSetting} />
+          <Menu theme={lightTheme} mode="inline" items={llmSetting} selectable={false} />
         </Sider>
         <Content style={{ width: '100vw', height: '100vh' }}>
           <div className="chat_background">
