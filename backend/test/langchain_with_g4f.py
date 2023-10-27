aedbf5dde8df6af6cf93b834bd1ed5c0dfe43439
Ludobico <aqs450@gmail.com>
2023-10-27 11:39:43 +0900

    test : async connection test

--- backend/test/langchain_with_g4f.py
+++ backend/test/langchain_with_g4f.py
@@ -1,5 +1,4 @@
-from typing import Optional, List, Mapping, Any
-from langchain.callbacks.manager import CallbackManagerForLLMRun
+from typing import Optional, List, Mapping, Any, AsyncIterable
 from langchain.llms.base import LLM
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
@@ -7,6 +6,8 @@ import g4f
 
 from functools import partial
 from langchain.callbacks.manager import AsyncCallbackManagerForLLMRun
+from langchain.callbacks import AsyncIteratorCallbackHandler
+import asyncio
 
 class TestLLM(LLM):
   @property
@@ -33,17 +34,62 @@ class TestLLM(LLM):
           text_callback = partial(run_manager.on_llm_new_token)
       
       text = ""
-      for token in g4f.ChatCompletion.create(model=g4f.models.gpt_4,messages=[{"role": "user", "content": prompt}], stream=True):
+      for token in g4f.ChatCompletion.create(model=g4f.models.gpt_35_turbo,provider=g4f.Provider.GptGo,messages=[{"role": "user", "content": prompt}], stream=True):
           if text_callback:
               await text_callback(token)
           text += token
           print(text)
       return text
 
-llm = TestLLM()
-prompt = PromptTemplate(
-  input_variables=['product'],
-  template="what is a good name for a company that makes {product}?",
-)
+# llm = TestLLM()
+# prompt = PromptTemplate(
+#   input_variables=['product'],
+#   template="what is a good name for a company that makes {product}?",
+# )
+# chain = LLMChain(llm=llm, prompt=prompt)
 
-chain = LLMChain(llm=llm, prompt=prompt)
+# async def async_generate() -> AsyncIterable[str]:
+#   callback = AsyncIteratorCallbackHandler()
+#   prompt = PromptTemplate(
+#   input_variables=['product'],
+#   template="what is a good name for a company that makes {product}?",
+#   )
+#   llm = TestLLM()
+#   chain = LLMChain(prompt=prompt, llm=llm, callbacks=[callback], verbose=True)
+
+#   task = asyncio.create_task(chain.arun("Google"))
+#   try:
+#      async for token in callback.aiter():
+#         yield token
+#   except Exception as e:
+#     print(f"Caught exception: {e}")
+#   finally:
+#     callback.done.set()
+  
+#   await task
+
+# if __name__ == "__main__":
+#   loop = asyncio.get_event_loop()
+#   async def main():
+#     async for token in async_generate():
+#         print(token)
+#   loop.run_until_complete(main())
+#   loop.close()
+
+async def async_generate(chain):
+   resp = await chain.arun("Google")
+   print(resp)
+
+async def generate_concurrently():
+  llm = TestLLM()
+  prompt = PromptTemplate(
+  input_variables=['product'],
+  template="what is a good name for a company that makes {product}?",)
+  chain = LLMChain(llm=llm, prompt=prompt)
+  tasks = [async_generate(chain)]
+  await asyncio.gather(*tasks)
+
+if __name__ == "__main__":
+  loop = asyncio.get_event_loop()
+  loop.run_until_complete(generate_concurrently())
+  loop.close
