55c9edcf1d3c5640257d14630cdd7440207c9a92
Ludobico <aqs450@gmail.com>
2023-10-31 09:37:14 +0900

    refactor : clear files

--- backend/Utils/G4F_for_custom_chain.py
+++
@@ -1,5 +0,0 @@
-from typing import Any, List, Mapping, Optional, Union
-from langchain.callbacks.manager import CallbackManagerForLLMRun, AsyncCallbackManagerForLLMRun
-from langchain.llms.base import LLM
-
-from g4f.models import Model
