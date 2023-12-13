d5adfaf34a23d53e4990b1653e78742c986ce2e6
Ludobico <aqs450@gmail.com>
2023-12-13 17:44:04 +0900

    fix : python version issue fix

--- InstallKit.bat
+++ InstallKit.bat
@@ -1,3 +1,3 @@
 @echo off
 
-start cmd /k "cd ./frontend && call yarn install && cd .. && cd backend && python -m venv worldstory_backend && cd worldstory_backend/Scripts/ && activate && cd ../../../ && pip install -r requirements.txt && exit"
+start cmd /k "cd ./frontend && call yarn install && cd .. && cd backend && python -m venv worldstory_backend && cd worldstory_backend/Scripts/ && activate && cd ../../ && pip install -r requirements.txt && exit"
