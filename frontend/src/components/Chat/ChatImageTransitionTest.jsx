6a98e16c678b0136ca225b0ecf936fa7669c3b88
Ludobico <aqs450@gmail.com>
2024-01-15 17:27:07 +0900

    shader : image transition

--- frontend/src/components/Chat/ChatImageTransitionTest.jsx
+++ frontend/src/components/Chat/ChatImageTransitionTest.jsx
@@ -4,13 +4,19 @@ import cyberpunkimage from '../Static/chat_background/cyberpunk-city-buildings-a
 import westernimage from '../Static/chat_background/western.jpg';
 import apocalypseimage from '../Static/chat_background/Apocalypse.jpg';
 import * as THREE from 'three'
-import { OrbitControls, PerspectiveCamera, shaderMaterial } from '@react-three/drei';
+import { OrbitControls, PerspectiveCamera, shaderMaterial, useTexture } from '@react-three/drei';
 import { Canvas, extend, useFrame } from '@react-three/fiber';
 import glsl from "glslify";
+import gsap from 'gsap'
 
 const ImageTransitionMaterial = shaderMaterial(
   // uniforms
-  {dispFactor : 0},
+  {
+    dispFactor : 0,
+    currentImage : new THREE.Texture(),
+    nextImage : new THREE.Texture(),
+  
+  },
   // vertex
   glsl`
   varying vec2 vUv;
@@ -58,24 +64,42 @@ extend({
 const ImageTransition = ({ currentImage, nextImage}) => {
   const materialRef = useRef();
 
-  useFrame((state, delta) => {
-    materialRef.current.uniforms.dispFactor.value = (Math.sin(delta) /2)
-  });
+  // useFrame((state, delta) => {
+  //   materialRef.current.uniforms.dispFactor.value += delta * 0.1
+  // });
+
+  const ChangeImages = () => {
+    console.log(materialRef.current.uniforms.dispFactor.value)
+    gsap.to(materialRef.current.uniforms.dispFactor, {
+      value: 1,
+      duration: 1,
+      ease: "power2.out",
+    })
+  }
 
   return(
-    <mesh position={[0,0,0]}>
+    <mesh position={[0,0,0]} onClick={ChangeImages}>
       <planeGeometry args={[1,1]} />
       <imageTransitionMaterial ref={materialRef} currentImage={currentImage} nextImage={nextImage} />
     </mesh>
   )
 }
+
+const Scene = () => {
+  const backgroundList = useTexture([fantasyimage, cyberpunkimage, westernimage, apocalypseimage]);
+  return(
+    <>
+    <OrbitControls />
+    <PerspectiveCamera makeDefault position={[0,0,3]}/>
+    <ImageTransition currentImage={backgroundList[0]} nextImage={backgroundList[1]} />
+    <ambientLight intensity={3} />
+    </>
+  )
+}
 const ChatImageTransitionTest = () => {
   return (
 <Canvas style={{width : '100vw', height : '100vh'}}>
-  <OrbitControls />
-  <PerspectiveCamera makeDefault position={[0,0,3]}/>
-  <ImageTransition currentImage={fantasyimage} nextImage={cyberpunkimage} />
-  <ambientLight intensity={3} />
+<Scene />
 </Canvas>
   );
 };
